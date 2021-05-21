2510461d5ec4b06e6d3b7b53398d8515e054038c
Nikita <n.plaksin8@gmail.com>
2021-05-22 01:28:54 +0300

    add microstyle and commented version of image cutting in the middle layer

--- StyleGAN.py
+++ StyleGAN.py
@@ -42,11 +42,11 @@ def StyleGAN(const_shape, resolution, n_dense=2, n_styles=5, middle_input_synth=
         # if middle_input_synth is None:
         # Layers for 4x4 resolution.
         middle_input = None
-
+        style = W[:, 0, 0:2]
         y = None
-        x = ModConv2d_grouped(rank=2, name="4x4/Conv", kernel_size=3, sampling=None, filters=nf(1), padding='same')([const_layer, W[:, 0]])
+        x = ModConv2d_grouped(rank=2, name="4x4/Conv", kernel_size=3, sampling=None, filters=nf(1), padding='same')([const_layer, style])
         x = tf.reduce_sum(x*style_strength_maps[0], axis=1)
-        y = torgb(x, y, W[:, 0], res_name='4x4', is_grouped=True, style_strength_map=style_strength_maps[0])
+        y = torgb(x, y, style, res_name='4x4', is_grouped=True, style_strength_map=style_strength_maps[0])
         outs = []
 
         # Layers for >=8x8 resolutions.
@@ -58,14 +58,25 @@ def StyleGAN(const_shape, resolution, n_dense=2, n_styles=5, middle_input_synth=
             else:
                 is_grouped = False
                 style_strength_map = None
-            x = Block(x, latents=W[:, 0], fmaps=nf(res-2), res_name=f'{2 ** res}x{2 ** res}', is_grouped=is_grouped, style_strength_map=style_strength_map)
+            if res == 4:
+                print(W.shape)
+            if res == 7:
+                pass
+                style = W[:, 0, 0:3:1]
+                # x = x[:, :12, :, :]
+                # y = y[:, :12, :, :]
+                # x = tf.concat([x[:, 1:6, :, :], x[:, 12:15, :, :], x[:, 21:, :, :]], axis=1)
+                # y = tf.concat([y[:, 1:6, :, :], y[:, 12:15, :, :], y[:, 21:, :, :]], axis=1)
+                # x = tf.concat([x[:, 4:10, :, :], x[:, 16:23, :, :], x[:, 27:36, :, :], x[:, 41:, :, :]], axis=1)
+                # y = tf.concat([y[:, 4:10, :, :], y[:, 16:23, :, :], y[:, 27:36, :, :], y[:, 41:, :, :]], axis=1)
+            x = Block(x, latents=style, fmaps=nf(res-2), res_name=f'{2 ** res}x{2 ** res}', is_grouped=is_grouped, style_strength_map=style_strength_map)
             y = upsample(y, [1, 3, 3, 1], data_format, res_name=f'{2 ** res}x{2 ** res}')
-            y = torgb(x, y, W[:, 0], res_name=f'{2 ** res}x{2 ** res}', is_grouped=is_grouped, style_strength_map=style_strength_map)
+            y = torgb(x, y, style, res_name=f'{2 ** res}x{2 ** res}', is_grouped=is_grouped, style_strength_map=style_strength_map)
             outs.append(x)
             outs.append(y)
         return outs, middle_input
     #
-    input_z = tf.keras.layers.Input(shape=[n_styles, 512], name='input_z')
+    input_z = tf.keras.layers.Input(shape=[n_styles+1, 512], name='input_z')
     input_const = tf.keras.layers.Input(shape=[const_shape[0], const_shape[1], 512], name='input_const')
     input_truncation_psi = tf.keras.layers.Input(shape=[1,], name='input_truncation_psi')
 
